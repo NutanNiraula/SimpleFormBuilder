7e1f922540afa26922eeac39b9f67e9ebb6f2f13
Nutan Niraula <nutan.smartmobe@gmail.com>
2019-04-14 18:36:07 +0545

    Initial Commit

---
+++ SimpleFormBuilder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleFormBuilder
+//
+//  Created by Nutan Niraula on 4/14/19.
+//  Copyright © 2019 Nutan Niraula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleFormBuilderTests/SimpleFormBuilderTests.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleFormBuilderTests.swift
+//  SimpleFormBuilderTests
+//
+//  Created by Nutan Niraula on 4/14/19.
+//  Copyright © 2019 Nutan Niraula. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleFormBuilder
+
+class SimpleFormBuilderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
